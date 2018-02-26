81cc1557cb180600a6036c544174ab41fd256989
Justin Ryan <justin@jry.se>
2018-02-25 19:01:49 -0500

    html

--- display.go
+++ display.go
@@ -13,44 +13,64 @@ func printText(ss *StationSet, w io.Writer) {
 	now := time.Now()
 	nowUnix := now.Unix()
 
-	fmt.Fprintf(w, "As of %s\n\n", now.In(loc).Format("3:04:05"))
+	header := `
+<html><head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-size: 1.3em;
+  font-family: monospace;
+}
 
-	//fmt.Fprintf(w, "It is %d\n\n", now)
-	_ = `
-<html><head><style>
-body { font-size: 1.5em; }
+#time { margin: 0.5em; }
+
+.stop {
+  margin: 0.8em 0 0.2em 0.3em;
+  font-weight: 500;
+}
+
+.row span {
+  margin: 0.2em;
+  display: table-cell;
+  min-width: 1.8em;
+  width: 1.8em;
+}
+
+.line { font-weight: 600; }
+.arr { padding-right: .2em; }
 </style></head><body>
 `
-	//	fmt.Fprintln(w, header)
+	fmt.Fprintln(w, header)
+	fmt.Fprintf(w, "<div id='time'>As of %s</div>\n", now.In(loc).Format("3:04:05"))
 
 	// TODO sort or weight
 	for _, s := range ss.Stations {
-		fmt.Fprintln(w, s.Name)
+		fmt.Fprintf(w, "<div class='stop'>%s</div>\n", s.Name)
 
 		for line, times := range s.NorthTimes {
-			fmt.Fprintf(w, "  N  %s ", line)
+			fmt.Fprintf(w, "<div class='row'><span class='dir'>N</span><span class='line'>%s</span>", line)
 			// TODO sort
 			for i, t := range unixToRelative(nowUnix, times) {
-				fmt.Fprintf(w, " %d", t)
+				fmt.Fprintf(w, "<span class='arr'>%d</span>", t)
 				if i > 8 {
 					break
 				}
 			}
-			fmt.Fprintln(w)
+			fmt.Fprintln(w, "</div>\n")
+
 		}
 		for line, times := range s.SouthTimes {
-			fmt.Fprintf(w, "  S  %s ", line)
+			fmt.Fprintf(w, "<div class='row'><span class='dir'>S</span><span class='line'>%s</span>", line)
 			for i, t := range unixToRelative(nowUnix, times) {
-				fmt.Fprintf(w, " %d", t)
+				fmt.Fprintf(w, "<span class='arr'>%d</span>", t)
 				if i > 8 {
 					break
 				}
 			}
-			fmt.Fprintln(w)
+			fmt.Fprintln(w, "</div>\n")
 		}
-		fmt.Fprintln(w)
 	}
-	//	fmt.Fprintln(w, "</body></html>")
+	fmt.Fprintln(w, "</body></html>")
 }
 
 func unixToRelative(now int64, ts []int64) []int {
